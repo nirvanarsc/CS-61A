ad8843d619bc18b484ba3c1e2aadd09d7e541854
nirvana_rsc <nirvana_rsc@yahoo.com>
2018-06-30 19:00:33 +0900

    lab3

---
+++ lab3/sort.scm
@@ -0,0 +1,10 @@
+(define (sort sent)
+  (if (empty? sent)
+    '()
+    (insert (first sent)
+            (sort (bf sent)))))
+
+(define (insert num sent)
+  (cond ((empty? sent) (se num))
+        ((< num (first sent)) (se num sent))
+        (else (se (first sent) (insert num (bf sent))))))
