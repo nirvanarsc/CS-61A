cf008d7f8d2f9d6313b53e4c63c2365d1a8b0cc2
nirvana_rsc <nirvana_rsc@yahoo.com>
2018-08-04 19:09:54 +0900

    lab3-finished

--- lab3/type-check.scm
+++ lab3/type-check.scm
@@ -1,7 +1,22 @@
-(define (type-check f arg check)
-  if (check f arg) #t
-  else #f
-  )
+;textbook solution - only check whether argument is a number
+;ex. 4
+(define (type-check1 f g arg)
+  (if (g arg) (f arg) #f))
+
+;ex. 5
+(define (make-safe1 f g)
+  (lambda (x)
+    (if (g x) (f x) #f)))
+
+;solution that checks if an error is thrown
+;ex. 4
+(define (type-check2 f arg)
+  (if (not(check f arg)) (f arg) "Invalid argument"))
 
 (define (check f arg)
-  (if (f arg) ZZ
+  (catch (f arg)))
+
+;ex. 5
+(define (make-safe2 f)
+  (lambda (x)
+    (if (not(check f x)) (f x) "Invalid argument")))
