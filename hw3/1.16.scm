014df53d435c9fe108c44c8da307d5a2f8bcac2b
nirvana_rsc <nirvana_rsc@yahoo.com>
2018-08-05 20:54:05 +0900

    SICP 1.16

--- hw3/1.16.scm
+++ hw3/1.16.scm
@@ -1,1 +1,19 @@
-###TODO
+(define (fib n)
+  (define (fib-iter a b count)
+    (if (= count 0)
+      b
+      (fib-iter (+ a b) a (- count 1))))
+  (fib-iter 1 0 n)
+  )
+
+(define (fast-expt b n)
+  (define (square x) (* x x))
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
+
+(define (expt-iter b n a)
+  (define (square x) (* x x))
+  (cond ((= n 0) a)
+        ((even? n) (expt-iter (square b) (/ n 2) a))
+        (else (expt-iter b (- n 1) (* a b)))))
