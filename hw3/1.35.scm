3f9cb6c1c550bb60eb5af555e3ebab461873269e
nirvana_rsc <nirvana_rsc@yahoo.com>
2018-08-05 21:07:00 +0900

    SICP 1.35

---
+++ hw3/1.35.scm
@@ -0,0 +1,21 @@
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+        next
+        (try next))))
+  (try first-guess))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+;sqrt in terms of fixed-point
+(define (sqrt x)
+  (fixed-point (lambda (y) (average y (/ x y))) 1.0))
+
+;golden ratio in terms of fixed-point
+(define (golden-ratio)
+  (fixed-point (lambda (x) (/ (+ x 1) x)) 1.0))
