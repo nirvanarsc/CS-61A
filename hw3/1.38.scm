30dfbb15e06628db1bcf6577bb6dc6b9522fb049
nirvana_rsc <nirvana_rsc@yahoo.com>
2018-08-12 20:19:34 +0900

    SICP 1.38

---
+++ hw3/1.38.scm
@@ -0,0 +1,19 @@
+(define (cont-fact n d k)
+  (define (recur i)
+    (if (> i k)
+      0
+      (/ (n i) (+ (d i) (recur (+ 1 i))))))
+  (recur 1))
+
+
+(define (cont-frac-golden-ratio k)
+  (cont-fact (lambda (i) 1.0) (lambda (i) 1.0) k))
+
+(define (cont-frac-e k)
+  (cont-fact (lambda (i) 1.0) (lambda (i)
+                                (if (= 2 (modulo i 3))
+                                  (inc-2 i)
+                                  1)) k))
+
+(define (inc-2 i)
+  (- i (/ (- i 2) 3)))
